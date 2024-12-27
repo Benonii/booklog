8ab0711322871816d45792a94db30cbb6bf65692
BENONI ESCKINDER <besckinder@gmail.com>
2024-12-28 00:30:21 +0300

    Signup and basic login complete(no JWT)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable validation pipe globally
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Set global prefix
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
