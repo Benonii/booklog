4f2e26420fe5819098518354242fdd5d0232af8b
BENONI ESCKINDER <you@example.com>
2024-12-28 03:07:38 +0300

    Book reltated endpoints implemented

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Get, Req, UseGuards, Patch, Delete } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
-import {AuthGuard} from '../auth/auth.guard';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('book')
 export class BookController {
@@ -12,6 +13,38 @@ export class BookController {
     async createBook(@Req() req, @Body() createBookDto: CreateBookDto) {
         const user_id = req.user.id;
         const result = await this.bookService.createBook(user_id, createBookDto);
-        return result
+        return result;
+    }
+
+    @Get()
+    @UseGuards(AuthGuard)
+    async getBooksByUser(@Req() req: any) {
+        const user_id = req.user.id;
+        const result = await this.bookService.getBooks(user_id);
+        return result;
+    }
+
+    @Get(':book_id')
+    @UseGuards(AuthGuard)
+    async getBookByID(@Req() req) {
+        const book_id = req.params['book_id'];
+        const result = await this.bookService.getBookByID(book_id);
+        return result;
+    }
+
+    @Patch(':book_id')
+    @UseGuards(AuthGuard)
+    async updateBook(@Req() req, @Body() updateBookDto: UpdateBookDto) {
+        const book_id = req.params['book_id'];
+        const result = await this.bookService.updateBook(book_id, updateBookDto);
+        return result;
+    }
+
+    @Delete(':book_id')
+    @UseGuards(AuthGuard)
+    async deleteBook(@Req() req) {
+        const book_id = req.params['book_id'];
+        const result = await this.bookService.deleteBook(book_id);
+        return result;
     }
 }
